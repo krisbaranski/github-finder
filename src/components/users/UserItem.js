5535fd5c2677e12f2d9ac719e976a2e5a7bc9f8a
krzysztof baranski <kb@krzysztofs-Mini.fritz.box>
2022-01-13 22:28:32 +0100

    feat: UserItem this.state props restructuring

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
+  state = {
+    id: "id",
+    login: "mojombo",
+    avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+    html_url: "https://github.com/mojombo",
+  };
+
   render() {
-    return <div>User Item</div>;
+    const { login, avatar_url, html_url } = this.state;
+    return (
+      <div className='card text-center'>
+        <img
+          src={avatar_url}
+          className='round-img'
+          alt='avatar'
+          style={{ width: "60px" }}
+        />
+        <h3>{login}</h3>
+        <div>
+          <a href={html_url} className='btn btn-dark btn-sm my-1'>
+            More
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
