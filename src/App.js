409a73025aebd341e07947240e22821cfec4f56b
krzysztof baranski <kb@krzysztofs-Mini.fritz.box>
2022-01-13 19:42:02 +0100

    feat: Add Navbar, UserItem components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
+import Navbar from "./components/layouts/Navbar";
+import UserItem from "./components/users/UserItem";
 import "./App.css";
 
 class App extends Component {
   render() {
-    const name = "John Doe";
     return (
       <div className='App'>
-        <h1>Hello {name}</h1>
+        <Navbar />
+        <UserItem />
       </div>
     );
   }
