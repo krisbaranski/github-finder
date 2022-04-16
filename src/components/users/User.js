42fd53cca04c619791a2737f7883945e449d7151
krzysztof baranski <krisbaranski@gmail.com>
2022-04-16 18:08:30 +0200

    feat: Correct minor bugs

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -3,7 +3,6 @@ import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 import { Link } from 'react-router-dom';
 import GithubContext from '../../context/github/githubContext';
-import githubContext from '../../context/github/githubContext';
 
 const User = ({ match }) => {
   const githubContext = useContext(GithubContext);
