64766233ca745df21c09224d05f335402ca0ff94
krzysztof baranski <krisbaranski@gmail.com>
2022-04-23 18:06:15 +0200

    feat: Correct minor bugs

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -12,6 +12,7 @@ const User = ({ match }) => {
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
+    // eslint-disable-next-line
   }, []);
 
   const {
--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react';
 import AlertContext from './alertContext';
 import AlertReducer from './alertReducer';
-import { SET_ALERT, CLEAR_USERS, REMOVE_ALERT } from '../types';
+import { SET_ALERT, REMOVE_ALERT } from '../types';
 
 const AlertState = (props) => {
   const initialState = null;
